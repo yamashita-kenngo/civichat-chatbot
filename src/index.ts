6151c8eaa925525dea5456cd169ce7efac20b973
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2021-08-18 16:43:11 +0900

    空白削除

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,6 @@ if (!process.env.LINE_CHANNEL_ACCESS_TOKEN) {
   );
 }
 
-
 const config = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.LINE_CHANNEL_SECRET,
