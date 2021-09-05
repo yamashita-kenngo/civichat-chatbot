1530571c3b061de012e6c537ac8e5de61eb34fed
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2021-09-05 18:36:37 +0900

    エラーハンドリング

--- db/index.js
+++ db/index.js
@@ -80,7 +80,8 @@ var pgConfig = {
 };
 console.log(pgConfig);
 var pg = new Client(pgConfig);
-pg.connect().then(function () { return console.log("pg Connected successfuly"); });
+pg.connect()
+    .then(function () { return console.log("pg Connected successfuly"); })["catch"](function () { return console.log("pr err"); });
 exports.getServiceDetail = function (serviceId) { return __awaiter(void 0, void 0, void 0, function () {
     var tableName, res, service;
     return __generator(this, function (_a) {
